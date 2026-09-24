6db38f81505505add06b2a2a3c56beb5158af647
guocheng1378 <guocheng1378@users.noreply.github.com>
2026-09-24 15:09:53 +0800

    chore: 清理临时脚本

--- _dep5.ps1
+++
@@ -1,5 +0,0 @@
-﻿Copy-Item 'C:\Users\Administrator\Desktop\全能外设\build_host\Release\apxdesktop.exe' 'C:\Users\Administrator\AppData\Local\Programs\AllPeriph\apxdesktop.exe' -Force
-(Get-Item 'C:\Users\Administrator\AppData\Local\Programs\AllPeriph\apxdesktop.exe').LastWriteTime = Get-Date
-Start-Process 'C:\Users\Administrator\AppData\Local\Programs\AllPeriph\apxdesktop.exe'
-Start-Sleep 2
-Get-Process apxdesktop -ErrorAction SilentlyContinue | Select-Object Id | Format-Table -AutoSize
