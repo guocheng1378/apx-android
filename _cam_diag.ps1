05a07aa2bc7ba1acf93ad8ba0e8e1194b4262a47
guocheng1378 <guocheng1378@users.noreply.github.com>
2026-09-24 16:00:52 +0800

    chore: 清理临时脚本

--- _cam_diag.ps1
+++
@@ -1,7 +0,0 @@
-﻿$adb = "C:\Users\Administrator\Desktop\全能外设\.tools\android-sdk\platform-tools\adb.exe"
-"== 手机端 CameraModule 日志 =="
-& $adb -s 192.168.2.182:5555 logcat -d | Select-String 'CameraModule|Camera \d|apx-cam' | Select-Object -Last 12
-"== 手机端 MediaOut / 发送 =="
-& $adb -s 192.168.2.182:5555 logcat -d | Select-String 'MediaOut|STREAM_CAMERA|摄像头' | Select-Object -Last 8
-"== 连接状态 =="
-cmd /c "netstat -ano | findstr 9502"
--- _verify_pc.ps1
+++
@@ -1,9 +0,0 @@
-﻿"== 构建产物 / 部署目标 / 源码 时间 =="
-Get-Item 'C:\Users\Administrator\Desktop\全能外设\build_host\Release\apxdesktop.exe',
-         'C:\Users\Administrator\AppData\Local\Programs\AllPeriph\apxdesktop.exe',
-         'C:\Users\Administrator\Desktop\全能外设\pc\host\src\media\h264_decoder.cpp' |
-    Select-Object Name,LastWriteTime,Length | Format-Table -AutoSize
-"== 运行中的面板 =="
-Get-Process apxdesktop -ErrorAction SilentlyContinue | ForEach-Object {
-    "pid=$($_.Id) start=$($_.StartTime) path=$($_.Path)"
-}
