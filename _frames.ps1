7662e69e269af7a82a0c4555da0ab89f71a1111c
guocheng1378 <guocheng1378@users.noreply.github.com>
2026-09-24 13:28:19 +0800

    chore: 清理临时脚本

--- _frames.ps1
+++
@@ -1,4 +0,0 @@
-﻿$adb = "C:\Users\Administrator\Desktop\全能外设\.tools\android-sdk\platform-tools\adb.exe"
-& $adb -s 694fc921 logcat -d -t 5 | Select-String '副屏页' 
-Start-Sleep 4
-& $adb -s 694fc921 logcat -d -t 5 | Select-String '副屏页'
--- _phone_look.ps1
+++
@@ -1,5 +0,0 @@
-$adb = "C:\Users\Administrator\Desktop\全能外设\.tools\android-sdk\platform-tools\adb.exe"
-# 显示器列表（多屏设备）
-& $adb -s 694fc921 shell dumpsys SurfaceFlinger --display-id
-# 当前前台 Activity
-& $adb -s 694fc921 shell dumpsys activity activities | Select-String 'topResumedActivity'
